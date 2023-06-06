4e4b20ac9dc76db0c67ba9a5e041f56b2cab06ee
Ilias A <iliasamri00@gmail.com>
2023-06-06 22:05:29 +0200

    First commit, scraps webpage and analyses it

---
+++ .history/scrapping_20230518153755.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+
+html = urllib.request.urlopen('https://laion.ai/blog/').read()
+text = text_from_html(html)
+print(text_from_html(html))
+
+
+file_path = "output.txt"
+
+# Open the file in write mode and write the text content
+with open(file_path, "w") as file:
+    file.write(text)
+
+print("Variable content saved to the file:", file_path)
